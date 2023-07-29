b7d2b30803f2fa7c2179c144321cba9fef8fc30f
“Engyousef7” <engyousef1997@gmail.com>
2023-07-29 18:45:10 +0300

    second commit

---
+++ task1.py
@@ -0,0 +1,15 @@
+name = "yousef"
+age = 26
+Birth_data  = 30/7/1997
+
+name = input("enter your name :")
+name = str(name)
+print(name)
+
+age = int(input("enter your Age :"))
+print(age)
+
+Birth_data = str(input("enter your Birth_data :"))
+print(Birth_data)
+
+

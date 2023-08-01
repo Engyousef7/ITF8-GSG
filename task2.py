26a53866f8623e83abb8f6194d2f2d26c8e5fab0
“Engyousef7” <engyousef1997@gmail.com>
2023-08-02 00:27:52 +0300

    second commit

---
+++ task2.py
@@ -0,0 +1,19 @@
+name = "yousef"
+mobile_number = "0592 742 846"
+Birth = 1997
+current = 2023
+user_age = current - Birth
+
+user_age = str(input("enter your user_age :"))
+print(user_age)
+
+name = input("enter your name :")
+name = str(name)
+print(name)
+
+print(mobile_number , split (""))
+
+n = 1997
+y = 2023
+m = n-y
+print(m)

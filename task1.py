380af138ab02428e629bcc93e2c756fd667d58ee
“Engyousef7” <engyousef1997@gmail.com>
2023-08-02 15:29:01 +0300

    second commit

--- task1.py
+++ task1.py
@@ -1,15 +1,15 @@
 name = "yousef"
-age = 26
-Birth_data  = 30/7/1997
+#age = 26
+#Birth_data  = 30/7/1997
 
-name = input("enter your name :")
-name = str(name)
+#name = input("enter your name :")
+#name = str(name)
 #print(name)
 
-age = int(input("enter your Age :"))
+#age = int(input("enter your Age :"))
 #print(age)
 
-Birth_data = str(input("enter your Birth_data :"))
+#Birth_data = str(input("enter your Birth_data :"))
 #print(Birth_data)
 
 
--- task2.1.py
+++ task2.1.py
@@ -1,15 +1,15 @@
 #name = "yousef"
 mobile_number = "0592 742 846"
-Birth = 1997
+year_of_birth = 1997
 current = 2023
-user_age = current - Birth
+user_age = current - year_of_birth
 
 full_name = input("enter your name :")
 mobile_number = input("enter your mobile_number :")
 year_of_birth = input("enter your year_of_birth :")
 print(full_name)
 print(user_age)
-print(mobile_number. spilt("-"))
+print(mobile_number,spilt("-"))
 
 #user_age = str(input("enter your user_age :"))
 #print(user_age)

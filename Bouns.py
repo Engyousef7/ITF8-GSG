14fee9232fa2385539908ba17a040a05a0889df1
“Engyousef7” <engyousef1997@gmail.com>
2023-08-05 01:49:58 +0300

    second commit

--- Bouns.py
+++ Bouns.py
@@ -1,6 +1,7 @@
 L1 = int(input("Enter L1"))
 L2 = int(input("Enter L2"))
 L3 = int(input("Enter L3"))
+L4 = int(input("Enter L4"))
 H = int(input("Enter H"))
 
 def Area(L1,H):
@@ -12,3 +13,14 @@ def perimeter(L1,L2,L3):
     perim = L1+L2+L3
     return perim
 print(perimeter(L1,L2,L3))
+
+
+def Area(L1,L2):
+    Area = L1*L2
+    return Area
+print(Area(L1,L2))
+
+def perimeter(L3):
+    perim = 4*L3
+    return perim
+print(perimeter(L3))

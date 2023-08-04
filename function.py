03c3bee329feddb853cc5979c7ae35b75e1ba802
“Engyousef7” <engyousef1997@gmail.com>
2023-08-04 20:40:08 +0300

    second commit

---
+++ function.py
@@ -0,0 +1,19 @@
+n1 = int(input("Enter n1"))
+n2 = int(input("Enter n2"))
+
+def add(n1,n2):
+    add = n1+n2
+    return add
+def subtract(n1,n2):
+    sub = n1+n2
+    return sub
+def multiplication(n1,n2):
+    multi = n1 * n2
+    return multi
+def division(n1,n2):
+    divi = n1/n2
+    return divi
+print(add(n1,n2))
+print(subtract(n1,n2))
+print(multiplication(n1,n2))
+print(division(n1,n2))
--- task2.py
+++ task2.py
@@ -9,7 +9,7 @@ mobile_number = input("enter your mobile_number :")
 year_of_birth = input("enter your year_of_birth :")
 print(full_name)
 print(user_age)
-print(mobile_number,spilt("-"))
+print(mobile_number.spilt("-"))
 
 #user_age = str(input("enter your user_age :"))
 #print(user_age)

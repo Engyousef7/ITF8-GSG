fa171ec059431649187f91f976fc2d4c9bd2647b
“Engyousef7” <engyousef1997@gmail.com>
2023-08-05 11:17:46 +0300

    second commit

--- Bouns.py
+++ Bouns.py
@@ -7,20 +7,25 @@ H = int(input("Enter H"))
 def Area(L1,H):
     Area = 0.5*L1*H
     return Area
-print(Area(L1,H))
+    if Area >= 10:
+        print("Area high")
+    elif Area <= 10:
+        print("Area low")
+
+#print(Area(L1,H))
 
 def perimeter(L1,L2,L3):
     perim = L1+L2+L3
     return perim
-print(perimeter(L1,L2,L3))
+#print(perimeter(L1,L2,L3))
 
 
 def Area(L1,L2):
     Area = L1*L2
     return Area
-print(Area(L1,L2))
+#print(Area(L1,L2))
 
 def perimeter(L3):
     perim = 4*L3
     return perim
-print(perimeter(L3))
+#print(perimeter(L3))
---
+++ if statment.py
@@ -0,0 +1,32 @@
+
+
+
+def Area(L1,H):
+    Area = 0.5*L1*H
+    if Area >= 10:
+         print("Area high")
+    elif Area <= 10:
+         print("Area low")
+    elif Area == 0:
+         print("Area invalid ")
+Area(2,5)
+
+def Area_circle(redius):
+    Area_circle = 3.14*(redius**2)
+    if Area_circle >= 10:
+         print("Area high")
+    elif Area_circle <= 10:
+         print("Area low")
+    elif Area == 0:
+         print("Area invalid ")
+Area_circle(5)
+
+def rectangle(L1,H):
+    Area = 0.5*L1*H
+    if rectangle >= 10:
+         print("Area high")
+    elif rectangle <= 10:
+         print("Area low")
+    elif rectangle == 0:
+         print("Area invalid ")
+rectangle(2,5)
